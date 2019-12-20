cf2b1465b236ceb19abf258197d49bc6260b8199
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-20 09:08:35 -0800

    aeson-johnson

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
-import CharacterList from "./components/CharacterList";
+import { Route, Link } from "react-router-dom";
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from "./components/WelcomePage.js"
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path ="/" component={WelcomePage} />
+      <CharacterList />
+      {/* <Route path= "/character" component={CharacterList} /> */}
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard({url, name, gender, species}) {
+  return (
+    <div>
+      <img src={url} />
+      <h2>{name}</h2>
+      <p>Gender: {gender}</p>
+      <p>Species: {species}</p>
+    </div>
+  );
 }
