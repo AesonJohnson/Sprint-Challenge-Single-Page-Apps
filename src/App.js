aedfec6408c066af0918e7c42a8db74aa2be3a4d
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-20 09:30:21 -0800

    aeson-johnson

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Header from "./components/Header.js";
 import { Route, Link } from "react-router-dom";
 import CharacterCard from "./components/CharacterCard.js"
 import CharacterList from "./components/CharacterList.js";
-import WelcomePage from "./components/WelcomePage.js"
+import WelcomePage from "./components/WelcomePage.js";
+import SearchForm from "./components/SearchForm.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Route path ="/" component={WelcomePage} />
-      <Route path= "/" component={CharacterList} />
-      <Route path="/card/:id" component={CharacterCard} />
-
+      <Link to="/">Home</Link>
+      <Link to="/characters">Characters</Link>
+      <Route exact path ="/" component={WelcomePage} />
+      <Route exact path="/card/:id" component={CharacterCard} />
+      <Route path= "/characters" component={CharacterList} />
     </main>
   );
 }
