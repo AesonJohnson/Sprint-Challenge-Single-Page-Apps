c7529e7111883b6c256e0ec325c86b0e64d5a970
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-20 10:11:22 -0800

    aeson-johnson

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import CharacterCard from "./components/CharacterCard.js"
 import CharacterList from "./components/CharacterList.js";
 import WelcomePage from "./components/WelcomePage.js";
 import SearchForm from "./components/SearchForm.js";
+import styles from "styled-components";
 
+const Links = styles.h3`
+  display:inline-block;
+  margin:10px;
+  color:black;
+`
 
 export default function App() {
   return (
     <main>
       <Header />
-      <Link to="/">Home</Link>
-      <Link to="/characters">Characters</Link>
+
+      <Link to="/"><Links>Home</Links></Link>
+      <Link to="/characters"><Links>Characters</Links></Link>
+
+      <SearchForm />
+
       <Route exact path ="/" component={WelcomePage} />
       <Route exact path="/card/:id" component={CharacterCard} />
       <Route path= "/characters" component={CharacterList} />
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [searchResults, setSearchResult] = useState(characters);
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    const results = characters.filter(character => {
+      return character.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setSearchResult(results);
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  }
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+  }
  
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={event => handleSubmit(event)}>
+          <input
+            id="character"
+            type="text"
+            name="character"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+      </form>
+
+      {searchResults.map((card, index) => (
+        <CharacterCard
+          key={index}
+          url={card.image}
+          name={card.name}
+          gender={card.gender}
+          species={card.species}
+        />
+      ))}
     </section>
   );
 }
